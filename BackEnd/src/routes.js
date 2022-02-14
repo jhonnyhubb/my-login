171774bf19f1f811764ae9a0e5c6776ad3d0d60d
Jonas Felix <jonas.medeiros.felix@gmail.com>
2022-02-14 15:49:15 -0300

    add jwt

--- BackEnd/src/routes.js
+++ BackEnd/src/routes.js
@@ -1,14 +1,24 @@
 import { Router } from 'express';
 
+import auth from './middlewares/auth'
+
 import HelloController from './controllers/HelloController';
+import SessionsController from './controllers/SessionsController';
 import UsersController from './controllers/UsersController';
 import RepositoriesController from './controllers/RepositoriesController';
 
+// public
+
 const routes = new Router();
+
+routes.post('/sessions', SessionsController.create);
 
 routes.get('/hello', HelloController.index);
 
-//CRUD RESTFull
+// middlewares
+routes.use(auth);
+
+// private
 routes.get('/users', UsersController.index);
 routes.get('/users/:id', UsersController.show);
 routes.post('/users', UsersController.create);
