c882435b6d4ab847ab038b0dc3c359fde8f7b9a6
Jonas Felix <jonas.medeiros.felix@gmail.com>
2022-02-28 11:33:39 -0300

    Front End

--- BackEnd/src/services/auth.js
+++
@@ -1,7 +0,0 @@
-import bcrypt from 'bcryptjs';
-
-export const createPasswordHash = async (password) => 
-    bcrypt.hash(password, 8);
-
-export const checkPassword = async (user, password) => 
-    bcrypt.compare(password, user.password);
